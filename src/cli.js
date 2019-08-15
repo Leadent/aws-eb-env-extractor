be6e5830269b360dc26d0e5409ae9ef0d9892f5c
Ross Coundon <ross.coundon@leadent.digital>
2019-08-15 14:40:19 +0100

    Initial commit

---
+++ src/cli.js
@@ -0,0 +1,56 @@
+import fs from 'fs';
+import util from 'util'
+import arg from 'arg';
+
+const exec = util.promisify(require('child_process').exec);
+
+
+function parseArgumentsIntoOptions(rawArgs) {
+  const args = arg(
+    {
+      '--env': String,
+      '--app': String,
+      '--output-file': String
+    },
+    {
+      argv: rawArgs.slice(2),
+    }
+  );
+  if(!args['--env']) throw new Error('--env is mandatory');
+  if(!args['--app']) throw new Error('--app is mandatory');
+  if(!args['--output-file']) throw new Error('--output-file is mandatory');
+  return{
+    environmentName: args['--env'],
+    applicationName: args['--app'],
+    outputFile: args['--output-file']
+  }
+ }
+
+function convertToEnv(envObj, outputFileName){
+  console.log(outputFileName)
+  envObj.forEach(entry => {
+    fs.appendFile(outputFileName, `${entry.OptionName}=${entry.Value}\n`, err => {
+    if (err) console.log(err);
+    });
+  });
+}
+
+async function retrieveEnvData(env, app, returnJson = true){
+  const {stdout: data} = await exec(`aws elasticbeanstalk describe-configuration-settings --environment-name ${env} --application-name ${app}`);
+  if(returnJson){
+    return JSON.parse(data);
+  }
+  return data;
+}
+
+export function cli (){
+
+  const cliOptions = parseArgumentsIntoOptions(process.argv);
+  retrieveEnvData(cliOptions.environmentName, cliOptions.applicationName, true).then((data) => {
+  const options = data.ConfigurationSettings[0].OptionSettings;
+  const envVars = options.filter(option => {
+    return option.Namespace === 'aws:elasticbeanstalk:application:environment';
+  })
+  convertToEnv(envVars, cliOptions.outputFile);
+})
+}
