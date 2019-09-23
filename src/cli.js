f0bab1eebfe3b464b6f25f7dc4d92025e4aab197
Ross Coundon <ross.coundon@leadent.digital>
2019-09-23 14:54:18 +0100

    Support for region

--- src/cli.js
+++ src/cli.js
@@ -4,13 +4,13 @@ import arg from 'arg';
 
 const exec = util.promisify(require('child_process').exec);
 
-
 function parseArgumentsIntoOptions(rawArgs) {
   const args = arg(
     {
       '--env': String,
       '--app': String,
-      '--output-file': String
+      '--output-file': String,
+      '--region': String
     },
     {
       argv: rawArgs.slice(2),
@@ -22,7 +22,8 @@ function parseArgumentsIntoOptions(rawArgs) {
   return{
     environmentName: args['--env'],
     applicationName: args['--app'],
-    outputFile: args['--output-file']
+    outputFile: args['--output-file'],
+    region: args['--region'] || 'eu-west-2'
   }
  }
 
@@ -35,8 +36,8 @@ function convertToEnv(envObj, outputFileName){
   });
 }
 
-async function retrieveEnvData(env, app, returnJson = true){
-  const {stdout: data} = await exec(`aws elasticbeanstalk describe-configuration-settings --environment-name ${env} --application-name ${app}`);
+async function retrieveEnvData(env, app, region, returnJson = true){
+  const {stdout: data} = await exec(`aws elasticbeanstalk describe-configuration-settings --environment-name ${env} --application-name ${app} --region ${region}`);
   if(returnJson){
     return JSON.parse(data);
   }
@@ -46,11 +47,11 @@ async function retrieveEnvData(env, app, returnJson = true){
 export function cli (){
 
   const cliOptions = parseArgumentsIntoOptions(process.argv);
-  retrieveEnvData(cliOptions.environmentName, cliOptions.applicationName, true).then((data) => {
-  const options = data.ConfigurationSettings[0].OptionSettings;
-  const envVars = options.filter(option => {
-    return option.Namespace === 'aws:elasticbeanstalk:application:environment';
+  retrieveEnvData(cliOptions.environmentName, cliOptions.applicationName, cliOptions.region, true).then((data) => {
+    const options = data.ConfigurationSettings[0].OptionSettings;
+    const envVars = options.filter(option => {
+      return option.Namespace === 'aws:elasticbeanstalk:application:environment';
+    })
+    convertToEnv(envVars, cliOptions.outputFile);
   })
-  convertToEnv(envVars, cliOptions.outputFile);
-})
 }
